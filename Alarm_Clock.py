f0758e2ff8b18536684ac39a2fb6ade9e6c4e968
decchu14 <1decchu14@gmail.com>
2021-05-04 01:37:15 +0530

    Adding alarm script

---
+++ Alarm_Clock.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from playsound import playsound
+
+alarm_time = input("Enter the time(HH:MM:SS AM or PM) to set: ")
+
+alarm_hour = alarm_time[0:2]
+alarm_min = alarm_time[3:5]
+alarm_sec = alarm_time[6:8]
+alarm_period = alarm_time[9:11].upper()
+
+print("setting up alarm...")
+
+while True:
+    now = datetime.now()
+    current_hour = now.strftime("%I")
+    current_min = now.strftime("%M")
+    current_sec = now.strftime("%S")
+    current_period = now.strftime("%p")
+
+    if(alarm_hour == current_hour):
+        if(alarm_min == current_min):
+            if(alarm_sec == current_sec):
+                if(alarm_period == current_period):
+                    print("Wake Up")
+                    playsound(
+                        'D:/python/UDEMYCOURSES/PYTHON/PYTHON_PROJECTS/5_Alarm_Clock/Audio.mp3')
+                    break
